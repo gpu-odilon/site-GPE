616540c94a8fb8927dbcea3e61740d17bcf1509d
gpu-odilon <gpuodilon@gmail.com>
2022-10-12 16:04:08 +0300

    commit version menu mobile

--- js/stiky-header.js
+++ js/stiky-header.js
@@ -1,5 +1,8 @@
 
 
+
+
+
 //*   start sticker header top
 window.onscroll = function() {myFunction()};
 
@@ -17,6 +20,45 @@ function myFunction() {
     document.getElementById("myHeader").style.padding = "30px 5% 40px 5%";
     document.getElementById("drop-subNav").style.top = "90px";
   }
+}
+
+
+// Get the modal 2
+var modal2 = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("closed1")[0];
+
+btn.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
 
 
